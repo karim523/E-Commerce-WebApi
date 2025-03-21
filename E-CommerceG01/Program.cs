57c3bd5ed29d91ca5d76eee68776fa0b16486274
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-21 19:17:53 +0200

    7 - DbContext - Configurations

--- E-CommerceG01/Program.cs
+++ E-CommerceG01/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistance.Data;
+
 namespace E_CommerceG01
 {
     public class Program
@@ -13,6 +16,11 @@ namespace E_CommerceG01
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
---
+++ Infrastructure/persistance/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Persistance.Data.Configurations
+{
+    class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.ProductBrand)
+                .WithMany()
+                .HasForeignKey(p => p.BrandId);
+
+            builder.HasOne(p => p.ProductType)
+                .WithMany()
+                .HasForeignKey(p => p.TypeId);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
