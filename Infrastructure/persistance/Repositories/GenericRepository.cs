de778e1b80ce19eeef63bde9df90d22bba135617
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-21 21:17:48 +0200

    9 - GenericRepository

---
+++ Infrastructure/persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Contracts;
+using Persistance.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly AppDbContext _context;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(TEntity entity)=> await _context.Set<TEntity>().AddAsync(entity);
+
+        public void Delete(TEntity entity) => _context.Set<TEntity>().Remove(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool AsNoTracking)=> AsNoTracking?
+           await  _context.Set<TEntity>().AsNoTracking().ToListAsync() :
+           await _context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id) => await _context.Set<TEntity>().FindAsync(id);
+
+        public void Update(TEntity entity) => _context.Set<TEntity>().Update(entity);
+
+    }
+}
