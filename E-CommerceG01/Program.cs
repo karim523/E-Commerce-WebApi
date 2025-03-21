ddf0212f9ccdb3487c268bba66ace786226b03ab
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-21 21:06:10 +0200

    8 - DbIntializer [Data Seeding]

--- E-CommerceG01/Program.cs
+++ E-CommerceG01/Program.cs
@@ -1,12 +1,14 @@
 
+using Domain.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Persistance.Data;
+using Persistance.Data.DataSeeding;
 
 namespace E_CommerceG01
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -21,9 +23,9 @@ namespace E_CommerceG01
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
-
+            builder.Services.AddScoped<IDbIntializer, DbIntializer>();
             var app = builder.Build();
-
+            await IntializeDbAsync(app);    
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -39,6 +41,13 @@ namespace E_CommerceG01
             app.MapControllers();
 
             app.Run();
+
+            async Task IntializeDbAsync(WebApplication app)
+            {
+                using var scope = app.Services.CreateScope();
+                var dbIntializer = scope.ServiceProvider.GetRequiredService<IDbIntializer>();
+                await dbIntializer.IntializeAsync();
+            }
         }
     }
 }
---
+++ Infrastructure/persistance/Data/DataSeeding/DbIntializer.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Persistance.Data.DataSeeding
+{
+    public class DbIntializer : IDbIntializer
+    {
+        private readonly AppDbContext _context;
+
+        public DbIntializer(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task IntializeAsync()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Any())
+                {
+                    await _context.Database.MigrateAsync();
+                    if (!_context.ProductTypes.Any())
+                    {
+                        var typeData = await File.ReadAllTextAsync(@"..\Infrastructure\persistance\Data\DataSeeding\types.json");
+                        var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                        if (types is not null && types.Any())
+                        {
+                            await _context.AddRangeAsync(types);
+                            await _context.SaveChangesAsync();
+                        }
+
+                    }
+
+                    if (!_context.ProductBrands.Any())
+                    {
+                        var brandsData = await File.ReadAllTextAsync(@"..\Infrastructure\persistance\Data\DataSeeding\brands.json");
+                        var brands = JsonSerializer.Deserialize<List<ProductType>>(brandsData);
+                        if (brands is not null && brands.Any())
+                        {
+                            await _context.AddRangeAsync(brands);
+                            await _context.SaveChangesAsync();
+                        }
+
+                    }
+
+                    if (!_context.Products.Any())
+                    {
+                        var productsData = await File.ReadAllTextAsync(@"..\Infrastructure\persistance\Data\DataSeeding\products.json");
+                        var products = JsonSerializer.Deserialize<List<ProductType>>(productsData);
+                        if (products is not null && products.Any())
+                        {
+                            await _context.AddRangeAsync(products);
+                            await _context.SaveChangesAsync();
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
