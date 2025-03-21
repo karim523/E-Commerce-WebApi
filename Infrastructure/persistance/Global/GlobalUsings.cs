b5e8cf93a04faac7882035ca1934ae0c68ff4f0f
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-21 21:42:58 +0200

    10 - UnitOfWork

--- Infrastructure/persistance/Global/GlobalUsings.cs
+++ Infrastructure/persistance/Global/GlobalUsings.cs
@@ -2,3 +2,6 @@
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using System.Reflection;
+global using Domain.Contracts;
+global using Persistance.Data;
+global using System.Collections.Concurrent;
--- Infrastructure/persistance/Repositories/GenericRepository.cs
+++ Infrastructure/persistance/Repositories/GenericRepository.cs
@@ -1,10 +1,5 @@
 ﻿using Domain.Contracts;
 using Persistance.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistance.Repositories
 {
