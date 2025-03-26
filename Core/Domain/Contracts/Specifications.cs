21629283e07cc587b2e4843327ebc61936399d42
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-26 05:01:31 +0200

    05 Specifications

---
+++ Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> where T : class
+    {
+        public Expression<Func<T, bool>>? Criteria { get; }
+        public List<Expression<Func<T, object>>>? IncludeExpressions { get; } = new();
+ 
+        protected Specifications(Expression<Func<T, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected void AddInclude(Expression<Func<T, object>> expression)
+             => IncludeExpressions.Add(expression);
+    }
+}
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    public class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        public ProductWithBrandAndTypeSpecifications(int id)
+            :base(product=> product.Id == id)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+        public ProductWithBrandAndTypeSpecifications()
+       : base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+    }
+}
