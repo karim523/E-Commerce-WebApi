a25452d6a384f749e23aee39b6a695def526107a
karim523 <142221662+karim523@users.noreply.github.com>
2025-03-26 05:17:51 +0200

    06 Specifications Evaluator

---
+++ Infrastructure/persistance/Repositories/SpecificationEvaluator.cs
@@ -0,0 +1,19 @@
+﻿namespace Persistance.Repositories
+{
+    public static class SpecificationEvaluator
+    {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> inputQuery
+            ,Specifications<T> specifications)where T : class 
+        {
+            var query = inputQuery;
+            if (specifications.Criteria is not null)
+                query = query.Where(specifications.Criteria);
+
+            query = specifications.IncludeExpressions.Aggregate(
+                query,
+                (currentQuery,includeExpression)=> currentQuery.Include(includeExpression));
+
+            return query;
+        } 
+    }
+}
