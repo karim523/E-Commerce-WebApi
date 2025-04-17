de1abd7d948add74123f56d607bccd2e8ce51ae6
karim523 <142221662+karim523@users.noreply.github.com>
2025-04-17 19:30:11 +0200

    08 Implement authorization for Swagger

--- E-CommerceG01/Global/GlobalUsings.cs
+++ E-CommerceG01/Global/GlobalUsings.cs
@@ -21,3 +21,4 @@ global using Shared;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.IdentityModel.Tokens;
 global using System.Text;
+global using Microsoft.OpenApi.Models;
